a2f7bb66f4c3ee0dc413de9b249014c321dc800e
Sylvain Guiblain <sylvain.guiblain@gmail.com>
2024-01-31 00:14:34 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'offline';
-const OFFLINE_URL = 'index.htm';
+const OFFLINE_URL = 'index.html';
 
 self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Install');
